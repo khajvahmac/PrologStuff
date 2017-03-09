6ffe0487e258696979b9839adb5346ec9c0bdb80
Khazhak Vahanyan <vahanyan@openmailbox.org>
2017-03-09 21:48:06 +0400

    added draw

--- labyrinth2.pl
+++ labyrinth2.pl
@@ -27,4 +27,22 @@ solution(CurrentBlock, Goal, PathAlreadyPassed, [Goal|Solution]) :-
         nextPath(Path, CurrentBlock, [], NewPath),
         solutionImpl(Y, Goal, [CurrentBlock|PathAlreadyPassed], NewPath, NewQueue, Solution).
   
+iterWrite(E, E, _, _).
+iterWrite(Block, Exit, S, S) :- write('\n|'), iterWrite(Block, Exit, S, 0).
+iterWrite(Block, Exit, Size, Index) :- 
+    (((X is Block + Size, connected(Block, X); X is Block + Size, connected(X, Block)), write(' ');
+    write('_')), 
+    ((Y is Block + 1, connected(Block, Y); Y is Block + 1, connected(Y, Block)), write(' ');
+    write('|'))), F is Block + 1, Z is Index + 1, iterWrite(F, Exit, Size, Z).
 
+repeat(Str,1,Str).
+repeat(Str,Num,Res):-
+    Num1 is Num-1,
+    repeat(Str,Num1,Res1),
+    string_concat(Str, Res1, Res).
+
+draw() :- 
+    entrance(X),
+    exit(Y), Z is Y + 1,
+    size(S), repeat(' _', S, Res), write(Res), write('\n '),
+    iterWrite(X, Z, S, 0).
